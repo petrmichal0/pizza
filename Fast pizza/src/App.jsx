92667a06ea598cd58a2ded38525df2057098aa81
Petr Michal <petrmichal0@gmail.com>
2024-01-09 21:39:43 +0100

    Add react Router 6.4 Logic

--- Fast pizza/src/App.jsx
+++ Fast pizza/src/App.jsx
@@ -1,7 +1,8 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Error from "./ui/Error";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
@@ -10,6 +11,7 @@ import AppLayout from "./ui/AppLayout";
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -18,6 +20,8 @@ const router = createBrowserRouter([
       {
         path: "/menu",
         element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
--- Fast pizza/src/ui/AppLayout.jsx
+++ Fast pizza/src/ui/AppLayout.jsx
@@ -1,11 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 
+import Loader from "./Loader";
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
+
       <Header />
 
       <main>
